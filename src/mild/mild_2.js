b7008c0e77f5a30cd352c6c39343c4832213ebed
n8turner-unc <54559270+n8turner-unc@users.noreply.github.com>
2021-10-06 10:41:50 -0400

    first attempt at mild_2.js

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,7 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   return `type: '${typeof variable}'`, `value: ${variable}`;
 }
 
 
@@ -24,7 +24,11 @@ export function identifyVariable(variable) {
 
  */
 export function identifyArray(array) {
-
+   const a = [];
+   for (let i = 0; i < array.length; i++){
+      a.push(identifyVariable(array[i]));
+   }
+   return a;
 }
 
 /**
@@ -44,7 +48,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object[key];
 }
 
 /**
@@ -64,7 +68,14 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   var item = {};
+   const k = object.keys();
+   for (let i = 0; i < k.length; i++){
+      if(k[i]!==key){
+         item[k[i]]=object[k[i]];
+      }
+   }
+   return item;
 }
 
 /**
@@ -89,5 +100,18 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   var item = {};
+   const k = object.keys();
+   for (let i = 0; i < k.length; i++){
+      var add = True;
+      for (let j = 0; j < keyList.length; j++){
+         if(k[i]===keyList[j]){
+            var add = False;
+         }
+      }
+      if (add){
+         item[k[i]]=object[k[i]];
+      }
+   }
+   return item;
 }
