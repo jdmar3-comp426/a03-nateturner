bf336c8583fcfa01a1a172ec2a8b4c4330ed3b0c
n8turner-unc <54559270+n8turner-unc@users.noreply.github.com>
2021-11-06 18:06:48 -0400

    editing median method in medium

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,7 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    var mid = (len(array)/2)|0;
+    var mid = (array.length/2)|0;
     return array[mid];
 }
 
